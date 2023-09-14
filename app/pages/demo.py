7e16940199582083f132e473a37fd76d6dbd106a
poamen <poamen@appzonegroup.com>
2023-09-14 08:40:34 +0100

    updated requirements

--- app/pages/demo.py
+++ app/pages/demo.py
@@ -1,4 +1,11 @@
 import streamlit as st
-
+from streamlit_elements import elements
 
 st.title("Demo")
+
+with elements("media_player"):
+
+    from streamlit_elements import media
+
+    media.Player(
+        url="https://youtu.be/aKVRj2TByAs", controls=True)
